1e1418e01446446bc66bc1560f67b14717301ca7
Derek Ng <derekng330@gmail.com>
2020-11-20 23:29:57 -0800

    Finished ch 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Products from "./Products";
@@ -6,9 +6,19 @@ import Header from "./Header";
 import { Routes, Route } from "react-router-dom";
 import Detail from "./Detail";
 import Cart from "./Cart";
+import { queryByAttribute } from "@testing-library/react";
 
 export default function Product() {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem("cart")) ?? [];
+    } catch (error) {
+      //test
+      return [];
+    }
+  });
+
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart), [cart]));
 
   function addToCart(id, sku) {
     setCart((items) => {
@@ -22,19 +32,34 @@ export default function Product() {
       }
     });
   }
+
+  function updateQuantity(sku, quantity) {
+    setCart((items) => {
+      if (quantity === 0) {
+        return items.filter((i) => i.sku !== sku);
+      } else {
+        return items.map((i) =>
+          i.sku === sku ? { ...i, quantity: quantity } : i
+        );
+      }
+    });
+  }
   return (
     <>
       <div className="content">
         <Header />
         <main>
           <Routes>
-            <Route path="/" element={<h1>Welcome to Carved RockFitness</h1>} />
+            <Route path="/" element={<h1>Welcome to Carved Rock Fitness</h1>} />
             <Route path="/:category" element={<Products />} />
             <Route
               path="/:category/:id"
               element={<Detail addToCart={addToCart} />}
             />
-            <Route path="/cart" element={<Cart />} />
+            <Route
+              path="/cart"
+              element={<Cart cart={cart} updateQuantity={updateQuantity} />}
+            />
           </Routes>
         </main>
       </div>
